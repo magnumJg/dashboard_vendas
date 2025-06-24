1d676a9ba370f22830e15859c8e1347cf64b4ffa
Jean <jeang10.goncalves@gmail.com>
2025-06-24 18:24:13 -0300

    Create Dados brutos.py

---
+++ pages/Dados brutos.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+import time
+
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index = False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon = "✅")
+    time.sleep(5)
+    sucesso.empty()
+
+
+
+st.title('DADOS BRUTOS')
+
+# Dicionário de regiões por UF
+regioes_brasil = {
+    'Norte': ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO'],
+    'Nordeste': ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE'],
+    'Centro-Oeste': ['DF', 'GO', 'MT', 'MS'],
+    'Sudeste': ['ES', 'MG', 'RJ', 'SP'],
+    'Sul': ['PR', 'RS', 'SC']
+}
+
+def obter_regiao(uf):
+    for regiao, ufs in regioes_brasil.items():
+        if uf in ufs:
+            return regiao
+    return 'Indefinida'
+
+## Leitura
+dados = pd.read_json('dataset_vendas_v2.json')
+dados['Região'] = dados['Local de compra'].apply(obter_regiao)
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], errors='coerce', dayfirst=True, format = '%d/%m/%Y')
+dados = dados.dropna(subset=['Data da Compra'])
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+
+st.sidebar.title('Filtros')
+with st.sidebar.expander('Nome do produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
+with st.sidebar.expander('Categoria do produto'):
+    categoria = st.multiselect('Selecione as categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Frete da venda'):
+    min_frete = dados['Frete'].min()
+    max_frete = dados['Frete'].max()
+    frete = st.slider('Frete', min_frete, max_frete, (min_frete,max_frete))
+with st.sidebar.expander('Data da Compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+with st.sidebar.expander('Vendedor'):
+    vendedores = st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+with st.sidebar.expander('Local de compra'):
+    local_compra = st.multiselect('Selecione o local da compra', dados['Local de compra'].unique(), dados['Local de compra'].unique())
+with st.sidebar.expander('Avaliação da compra'):
+    avaliacao = st.slider('Selecione a avaliação da compra',1,5, value = (1,5))
+with st.sidebar.expander('Tipo de pagamento'):
+    tipo_pagamento = st.multiselect('Selecione o tipo de pagamento',dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
+with st.sidebar.expander('Quantidade de parcelas'):
+    max_parcelas = dados['Quantidade de parcelas'].max()
+    min_parcelas = dados['Quantidade de parcelas'].min()
+    qtd_parcelas = st.slider('Selecione a quantidade de parcelas', min_parcelas, max_parcelas, (min_parcelas,max_parcelas))
+
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+`Local de compra` in @local_compra and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
+
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility = 'collapsed', value = 'dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data = converte_csv(dados_filtrados), file_name = nome_arquivo, mime = 'text/csv', on_click = mensagem_sucesso)
